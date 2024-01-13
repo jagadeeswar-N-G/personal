6336b9de10377e96b369ca37c9657b2472db5219
jagadeeswar-N-G <jagadeeswar8055@gmail.com>
2024-01-13 23:25:54 +0530

    git ignore

--- .next/static/chunks/src_7d6123._.js
+++ .next/static/chunks/src_7d6123._.js
@@ -132,7 +132,7 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist
 ;
 const BackgroudText = ()=>{
     return /*#__PURE__*/ __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"]("div", {
-        className: "mt-4 text-7xl md:text-8xl text-gray-600 opacity-15",
+        className: "mt-4 text-6xl md:text-8xl text-gray-600 opacity-15",
         children: [
             /*#__PURE__*/ __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"]("p", {
                 children: "Let's"
--- src/components/BackgroudText.tsx
+++ src/components/BackgroudText.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BackgroudText = () => {
   return (
-    <div className="mt-4 text-7xl md:text-8xl text-gray-600 opacity-15">
+    <div className="mt-4 text-6xl md:text-8xl text-gray-600 opacity-15">
       <p>Let's</p>
       <p>Talk About</p>
       <p>Programming</p>
